6f306e6c9fb92b383f8f8bcc10429f10e850d079
Yusuf0665 <95577518+Yusuf0665@users.noreply.github.com>
2022-12-06 03:04:57 +0300

    Add project files.

---
+++ odev3_MyHome/MyHome.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace my_home
+{
+    public class MyHome
+    {
+        string isim;
+        byte yas;
+        string meslek;
+        byte boy;
+        byte kilo;
+        string cinsiyet;
+
+        public MyHome(string name, byte age, string job, byte height, byte weight, bool women)
+        {
+            isim = name;
+            yas = age;
+            meslek = job;
+            boy = height;
+            kilo = weight;
+
+            if (women == true)
+            {
+                cinsiyet = "Kadın";
+            }
+            else
+            {
+                cinsiyet = "Erkek";
+            }
+        }
+
+        public void bilgileriGoster()
+        {
+            Console.WriteLine("İsim: " + isim);
+            Console.WriteLine("Yaş: " + yas);
+            Console.WriteLine("Meslek: " + meslek);
+            Console.WriteLine("Boy: " + boy);
+            Console.WriteLine("Kilo: " + kilo);
+            Console.WriteLine("Cinsiyet: " + cinsiyet);
+        }
+    }
+}
---
+++ odev3_MyHome/Program.cs
@@ -0,0 +1,14 @@
+﻿using my_home;
+using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        MyHome ben = new MyHome("Joseph", 22, "Software Engineer", 183, 59, false);
+
+        ben.bilgileriGoster();
+
+        Console.ReadLine();
+    }
+}
